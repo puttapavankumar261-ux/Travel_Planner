784d19b2b7696a32bc8520f3323f6b3395946e54
hema <hemasri.learning@gmail.com>
2026-07-28 17:06:50 +0530

    Changes done for pull conflicts

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,5 @@
 import axiosInstance from "../utils/axiosInstance";
-import axios from 'axios'; 
-const api = axios.create({
-  baseURL: "http://localhost:8080/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+
 const login = async (loginData) => {
 
     const response = await axiosInstance.post(
@@ -15,15 +9,7 @@ const login = async (loginData) => {
 
     return response.data;
 };
-const sendOtp = async (otpData) => {
-  const response = await axiosInstance.post("/api/otp/send", otpData);
-  return response.data;
-};
 
-const verifyOtp = async (otpData) => {
-  const response = await axiosInstance.post("/api/otp/verify", otpData);
-  return response.data;
-};
 const register = async (registerData) => {
 
     const response = await axiosInstance.post(
@@ -34,11 +20,57 @@ const register = async (registerData) => {
     return response.data;
 };
 
-const authService = {
-  login,
-  register,
-  sendOtp,
-  verifyOtp,
+// const sendOtp = async (otp) => {
+
+//     const response = await axiosInstance.post(
+//         "/api/auth/send",
+//         otp
+//     );
+
+//     return response.data;
+// };
+
+const sendOtp = async (email) => {
+
+    const response = await axiosInstance.post(
+        "/api/otp/send",
+        {
+           "email" : email,
+            "purpose":"REGISTRATION"
+        }
+    );
+
+    return response.data;
 };
 
-export default authService;
+// const verifyOtp = async (otp) => {
+
+//     const response = await axiosInstance.post(
+//         "/api/auth/verify",
+//         otp
+//     );
+
+//     return response.data;
+// };
+
+const verifyOtp = async (email, otp) => {
+
+    const response = await axiosInstance.post(
+        "/api/otp/verify",
+        {
+            "email":email,
+            "otp": otp,
+            "purpose": "REGISTRATION"
+        }
+    );
+
+    return response.data;
+};
+
+
+export default {
+    login,
+    register,
+    sendOtp,
+    verifyOtp
+};
